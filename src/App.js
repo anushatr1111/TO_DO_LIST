52e41bf387baa91852ed00106a4c04c1a8269dcf
anushatr1111 <anusharajeshkumar08@gmail.com>
2025-03-16 15:58:48 +0530

    Fixed ignored files issue

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
